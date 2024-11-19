2da4f3e83ee7b109c77e55020e6316801efc4562
DarkMode08 <michaelrodriguez5067@gmail.com>
2024-11-19 06:45:15 -0400

    19/11/24 - Act Angular & HTML5

--- Portfolio/V0.0/src/app/app-routing.module.ts
+++ Portfolio/V0.0/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Comillas Vacias Para Especificar la Ruta Inicial.
+  { path: 'home', component: PortfolioComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'item', component: ItemComponent },
+  // ** Para cualquier otra ruta no especificada sera dirigida a esto.
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
